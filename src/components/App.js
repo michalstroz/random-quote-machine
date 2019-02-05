7df72ffd872197763afe94e482daa0c13d138efa
Michał Stróż <michalstroz@gmail.com>
2019-02-05 23:55:37 +0100

    using fetch method to get some quotes from api

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,15 @@ import '../styles/App.css';
 import QuoteBox from './QuoteBox';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch("https://big-okra.glitch.me/https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=41&callback=", {
+       cache: "no-cache"
+    })
+      .then(res => res.json())
+    	.then(result => console.log(result));
+  }
+
   render() {
     return (
       <div className="App">
