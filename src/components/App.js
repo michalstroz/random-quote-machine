827f404ea1e1a36dcbdbd9e68c5ce3f98eecf564
Michał Stróż <michalstroz@gmail.com>
2019-02-24 13:21:31 +0100

    set api result as a state and forward it to QuoteBox component

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,49 @@ import '../styles/App.css';
 import QuoteBox from './QuoteBox';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      quotes: []
+    }
+  }
 
   componentDidMount() {
     fetch("https://big-okra.glitch.me/https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=41&callback=", {
        cache: "no-cache"
     })
       .then(res => res.json())
-    	.then(result => console.log(result));
+    	.then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            quotes: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
   }
 
   render() {
-    return (
-      <div className="App">
-        <QuoteBox />
-      </div>
-    );
+    const {error, isLoaded, quotes} = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div className="App">
+          <QuoteBox quotes={quotes}/>
+        </div>
+      );
+    }
   }
 }
 
