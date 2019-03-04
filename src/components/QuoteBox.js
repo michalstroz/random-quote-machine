6078ef4973b77e4207f9f5f7ce185dc31a9ee235
Michał Stróż <michalstroz@gmail.com>
2019-03-04 22:04:21 +0100

    implement new fontawesome icons

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -9,15 +9,17 @@ const QuoteBox = (props) => {
 
   return (<article id="quote-box">
     <section className="quote-wrapper">
-      <p id="text">{quoteContent}</p>
+      <p id="text"><FontAwesomeIcon icon="quote-left" size="2x"/> {quoteContent}<FontAwesomeIcon icon="quote-right" size="2x"/></p>
       <p id="author">
         <span>- {quoteAuthor}</span>
       </p>
     </section>
-    <button id="new-quote" onClick={props.handleClick}>SPIN</button>
-    <a className="button" id="tweet-quote" href={createTwitterString(quoteContent, quoteAuthor)} target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={['fab', 'twitter-square']} size="4x"/>
-    </a>
+    <section className="buttons">
+      <button id="new-quote" onClick={props.handleClick}>SPIN</button>
+      <a className="button" id="tweet-quote" href={createTwitterString(quoteContent, quoteAuthor)} target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={['fab', 'twitter-square']} />
+      </a>
+    </section>
   </article>);
 }
 
