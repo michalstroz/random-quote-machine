7e6bcaf3f20f92c063cf9790f1e56775c9dcd119
Michał Stróż <michalstroz@gmail.com>
2019-03-13 23:00:09 +0100

    change colors of content after page load or click

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fab} from '@fortawesome/free-brands-svg-icons';
 import {faQuoteRight, faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
+import {randomColors} from './functions'
 import QuoteBox from './QuoteBox';
 import '../styles/App.scss';
 
@@ -15,23 +16,31 @@ class App extends Component {
       isLoaded: false,
       quotes: [],
       quote: {},
+      colors: [],
+      backgroundGradient: ''
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
+    const colors = randomColors();
     this.setState({
-      quote: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
+      quote: this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)],
+      colors: colors,
+      backgroundGradient: `linear-gradient(45deg, ${colors[0]}, ${colors[1]})`
     });
     event.preventDefault();
   }
 
   componentDidMount() {
+    const colors = randomColors();
     fetch("https://big-okra.glitch.me/https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=41&callback=", {cache: "no-cache"}).then(res => res.json()).then((result) => {
       this.setState({
         isLoaded: true,
         quotes: result,
-        quote: result[Math.floor(Math.random() * result.length)]
+        quote: result[Math.floor(Math.random() * result.length)],
+        colors: colors,
+        backgroundGradient: `linear-gradient(45deg, ${colors[0]}, ${colors[1]})`
       });
     }, (error) => {
       this.setState({isLoaded: true, error});
@@ -45,8 +54,8 @@ class App extends Component {
     } else if (!isLoaded) {
       return <div>Loading...</div>
     } else {
-      return (<div className="App">
-        <QuoteBox quote={this.state.quote} handleClick={this.handleClick}/>
+      return (<div className="App" style={{background: this.state.backgroundGradient}}>
+        <QuoteBox quote={this.state.quote} colors={this.state.colors} handleClick={this.handleClick}/>
       </div>);
     }
   }
