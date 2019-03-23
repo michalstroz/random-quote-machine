44a987fefdc8f2ad293cc5bad2c90943616f6264
Michał Stróż <michalstroz@gmail.com>
2019-03-23 16:34:36 +0100

    use styled-components keyframes to make change color animation

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,26 +2,51 @@ import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import '../styles/QuoteBox.scss';
 import {htmlDecode, createTwitterString} from './functions.js';
+import styled, {css, keyframes } from 'styled-components';
 
 const QuoteBox = (props) => {
   const quoteContent = htmlDecode(props.quote.content.replace(/<\/*p>/g, ""));
   const quoteAuthor = htmlDecode(props.quote.title);
-  const styles = [
-    {
-      boxShadow: `1px 1px 4px 0 ${props.colors[2]}`,
-      color: props.colors[1],
-    },
-    {
-      backgroundColor: props.colors[1],
-      color: '#ffffff'
-    },
-    {
-      color: props.colors[1]
+  const colors = props.colors;
+
+  const changeColor = keyframes`
+    0% {
+      color: ${colors.previousColor}
     }
-  ]
+    100% {
+      color: ${colors.currentCollor}
+    }
+  `;
+
+  const changeBackground = keyframes`
+    0% {
+      background-color: ${colors.previousColor}
+    }
+    100% {
+      background-color: ${colors.currentCollor}
+    }
+  `;
+
+  const animation = () => css`${changeColor} 1000ms ease-out`;
+
+  const QuoteBoxWrapper = styled.article`
+    animation: ${animation};
+    animation-fill-mode: forwards;
+    box-shadow: 1px 1px 4px 0 ${colors.boxShadow};
+  `;
+
+  const TweetQuote = styled.a`
+      animation: ${animation};
+      animation-fill-mode: forwards;
+  `;
+
+  const NewQuote = styled.button`
+      animation: ${changeBackground} 1000ms ease-out;
+      animation-fill-mode: forwards;
+  `;
 
   return (
-    <article id="quote-box" style={styles[0]}>
+    <QuoteBoxWrapper id="quote-box">
       <section className="quote-wrapper">
         <p id="text"><FontAwesomeIcon icon="quote-left" size="1x"/> {quoteContent}<FontAwesomeIcon icon="quote-right" size="1x"/></p>
         <p id="author">
@@ -29,12 +54,12 @@ const QuoteBox = (props) => {
         </p>
       </section>
       <section className="buttons">
-        <button id="new-quote"  onClick={props.handleClick} style={styles[1]}>SPIN</button>
-        <a className="button" id="tweet-quote" href={createTwitterString(quoteContent, quoteAuthor)} style={styles[2]} target="_blank" rel="noopener noreferrer">
+        <NewQuote id="new-quote"  onClick={props.handleClick}>SPIN</NewQuote>
+        <TweetQuote className="button" id="tweet-quote" href={createTwitterString(quoteContent, quoteAuthor)} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={['fab', 'twitter-square']} />
-        </a>
+        </TweetQuote>
       </section>
-    </article>
+    </QuoteBoxWrapper>
   );
 }
 
